bde47355f86420151e4242c50a4602f4c99aca05
Johann Berger <johann@objekt.stream>
2023-06-27 00:57:58 +0200

    ~

--- src/components/Solid.tsx
+++ src/components/Solid.tsx
@@ -2,9 +2,7 @@
 
 import { createSignal } from "solid-js"
 
-// simple solidjs hello world component
 export default function Solid() {
-  // count signal
   const [count, setCount] = createSignal(0)
   return <p onclick={() => setCount(count() + 1)}>Hello from Solid {count()}</p>
 }
