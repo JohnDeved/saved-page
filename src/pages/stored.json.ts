bc2b81d87db44e9d892fc77c3a8a9f585c3bc1ce
Johann Berger <johann@objekt.stream>
2023-06-28 11:22:46 +0200

    ~ move to npm module

--- src/pages/stored.json.ts
+++ src/pages/stored.json.ts
@@ -1,10 +1,10 @@
 import type { APIRoute } from 'astro'
-import s from '../../save/stored.json'
+import s from '@undefined/saved'
 
 export const stored = s.sort((a, b) => b.created - a.created)
 export type Stored = typeof stored[0]
 
-export const get: APIRoute = ({ params, request }) => {
+export const get: APIRoute = () => {
   return {
     body: JSON.stringify(stored),
   }
