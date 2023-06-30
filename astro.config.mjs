d6549d52897c9fb90b04c5767e0c4873cb313caf
Johann Berger <johann@objekt.stream>
2023-06-30 18:30:54 +0200

    + gen sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import solidJs from '@astrojs/solid-js'
 import pandacss from '@pandacss/dev/astro'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://reddit-saved.pages.dev',
   integrations: [
     react(),
     solidJs(),
     pandacss(),
+    sitemap()
   ],
 })
